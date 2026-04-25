5c13a5aaf97707b998fa3488d2d426559f6f2ce7
JihanGajjar <jihan.anil.gajjar27@gmail.com>
2026-04-25 12:57:33 +0530

    Frontend

--- Prepify-backend/src/index.js
+++
@@ -1,9 +0,0 @@
-import dotenv from "dotenv";
-import { connectDB } from "./database/db.connect.js";
-import server from "./server.js";
-dotenv.config();
-connectDB().then(() => {
-  server.listen(process.env.PORT, () => {
-    console.log(`Server is running on port ${process.env.PORT}`);
-  });
-});
