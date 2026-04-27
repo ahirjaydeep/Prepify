fdfaaac0ca47d05de74d2b8f97787235cda72826
Jaydeep Ahir <ahirjaydeep18@icloud.com>
2026-04-27 12:10:11 +0530

    Add new job routes for closing and user jobs

--- Prepify-backend/src/routes/job.route.js
+++ Prepify-backend/src/routes/job.route.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { createJob, getAllJobs, applyToJob, selectCandidate} from "../controllers/job.controller.js";
+import { createJob, getAllJobs, applyToJob, selectCandidate, closeJob, getUserJobs, getUserAppliedJob } from "../controllers/job.controller.js";
 
 const router = Router();
 
@@ -7,6 +7,8 @@ router.route("/all").get(getAllJobs);
 router.route("/create").post(createJob);
 router.route("/apply").post(applyToJob);
 router.route("/select-candidate").post(selectCandidate);
-
+router.route("/close").post(closeJob);
+router.route("/recruiter-jobs").post(getUserJobs);
+router.route("/applied-jobs").post(getUserAppliedJob);
 
 export { router as jobRouter };
